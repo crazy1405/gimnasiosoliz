706df94fb02e43539b6ce2a262427bacf59ae498
Verónica Vintimilla <33739940+vvintimilla@users.noreply.github.com>
2017-11-21 11:12:39 -0500

    Cliente DAO

---
+++ gimnasiosoliz/src/main/java/jorge/gimnasiosoliz/data/ClienteDAO.java
@@ -0,0 +1,38 @@
+package jorge.gimnasiosoliz.data;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import jorge.gimnasiosoliz.model.Categoria;
+import jorge.gimnasiosoliz.model.Cliente;
+
+@Stateless
+public class ClienteDAO 
+{
+	@Inject
+	//Gestor de Entidades - conexion JPA
+	private EntityManager em;
+	
+	public void insertar(Cliente cliente)
+	{
+		em.persist(cliente);
+	}
+	
+	public void actualizar(Cliente cliente)
+	{
+		em.merge(cliente);
+	}
+	
+	public Cliente leer(int cli_id)
+	{
+		em.find(Cliente.class, cli_id);
+		return null;
+	}
+
+	
+	public void borrar(int cli_id)
+	{
+		em.remove(leer(cli_id));
+	}
+}
